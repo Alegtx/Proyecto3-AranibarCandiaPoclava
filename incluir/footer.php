6ba6b9fa44730be121a1a3b593c680fcdae02eab
Alegtx <alejandroa453@gmail.com>
2020-10-30 14:34:39 -0400

    Include
    Pie de pagina, Barra de navegacion y las referencias a CSS, Js que se deben incluir mediante PHP a cada pagina
    
    Co-Authored-By: CH3ITO <49537021+CH3ITO@users.noreply.github.com>

---
+++ incluir/footer.php
@@ -0,0 +1,19 @@
+<footer>
+    <h3 class="text-center">Siguenos en</h3><br>
+    <ul class="list-unstyled text-center">
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Facebook">
+            <img src="assets/iconos/social-facebook.png" alt="facebook-icon">
+        </a>
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Twitter">
+            <img src="assets/iconos/social-twitter.png" alt="twitter-icon">
+        </a>
+    </ul>
+    <br>
+    <h4 class="text-center tittles-pages-logo">
+    ©2020 Shopon-line™, INC. TODOS LOS DERECHOS RESERVADOS.<br>
+    Todas las marcas registradas referenciadas son propiedad de sus respectivos dueños.
+    </h4>
+    <br>
+    <h5 class="text-center tittles-pages-logo">Shopon-line &copy; 2020™</h5>
+</footer>
