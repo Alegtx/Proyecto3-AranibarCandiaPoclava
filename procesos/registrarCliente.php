c2c48d578aad6414138b4bebaf1c34646cf22cfd
Alegtx <alejandroa453@gmail.com>
2020-12-04 23:10:35 -0400

    Captcha de registro
    -Si el usuario introduce mal el captcha o se el usuario o nit estan ocupados el numero de captcha cambiara
    
    Co-Authored-By: CH3ITO <49537021+CH3ITO@users.noreply.github.com>

--- procesos/registrarCliente.php
+++ procesos/registrarCliente.php
@@ -16,45 +16,77 @@
     if(!$nitCliente == "" && !$usuarioCliente == "" && !$apeCliente == "" && !$dirCliente == "" && !$phoneCliente == "" && !$emailCliente == "" && !$nombreCliente == "")
     {
         session_start();
+        //Comprobar captcha
         if($_SESSION['numeroaleatorio'] == $captchaCliente)
         {
-            $verificar =  ejecutarSQL::consultar("select * from cliente where NIT='".$nitCliente."'");
-            $verificaltotal = mysqli_num_rows($verificar);
-            if($verificaltotal <= 0)
+            //Comprobar usuario
+            $verificarUsuario = ejecutarSQL::consultar("select * from cliente, administrador  where cliente.Usuario = '".$usuarioCliente."' or administrador.Usuario = '".$usuarioCliente."'");
+            $usuariostotal = mysqli_num_rows($verificarUsuario);
+            if($usuariostotal <= 0)
             {
-                if(consultasSQL::InsertSQL("cliente", "NIT, Usuario, Nombre, Apellidos, Clave, Direccion, Telefono, Email", "'$nitCliente','$usuarioCliente','$nombreCliente','$apeCliente','$passCliente', '$dirCliente','$phoneCliente','$emailCliente'"))
+                //Comprobar NIT
+                $verificarNIT =  ejecutarSQL::consultar("select * from cliente where NIT = '".$nitCliente."'");
+                $verificaltotal = mysqli_num_rows($verificarNIT);
+                if($verificaltotal <= 0)
                 {
-                    echo '<img src="assets/img/ok.png" class="center-all-contens"><br>El registro se completo con éxito';
-                    $_SESSION['nombreUser'] = $usuarioCliente;
-                    $_SESSION['claveUser'] = $passCliente;
-                    echo '
-                        <br>
-                        <p><strong>Se le va a redirigir automaticamente.</strong></p>
-                        <script>
-                            setTimeout(function(){
-                            url ="index";
-                            $(location).attr("href",url);
-                            },3000);
-                        </script>
-                    ';
+                    if(consultasSQL::InsertSQL("cliente", "NIT, Usuario, Nombre, Apellidos, Clave, Direccion, Telefono, Email", "'$nitCliente','$usuarioCliente','$nombreCliente','$apeCliente','$passCliente', '$dirCliente','$phoneCliente','$emailCliente'"))
+                    {
+                        echo '<img src="assets/img/ok.png" class="center-all-contens"><br>El registro se completo con éxito';
+                        $_SESSION['nombreUser'] = $usuarioCliente;
+                        $_SESSION['claveUser'] = $passCliente;
+                        echo '
+                            <br>
+                            <p><strong>Se le va a redirigir automaticamente.</strong></p>
+                            <script>
+                                setTimeout(function(){
+                                url ="index";
+                                $(location).attr("href",url);
+                                },3000);
+                            </script>
+                        ';
+                    }
+                    else
+                    {
+                       echo '<img src="assets/img/error.png" class="center-all-contens"><br>Ha ocurrido un error.<br>Por favor intente nuevamente.'; 
+                    }
                 }
                 else
                 {
-                   echo '<img src="assets/img/error.png" class="center-all-contens"><br>Ha ocurrido un error.<br>Por favor intente nuevamente'; 
+                    echo '
+                        <img src="assets/img/error.png" class="center-all-contens"><br>El NIT que ha ingresado ya esta registrado.<br>Por favor ingrese otro nombre de usuario.
+                        <script>
+                            $("#img-captcha").attr("src","./captcha/Captcha.php");
+                        </script>
+                        ';
                 }
             }
             else
             {
-                echo '<img src="assets/img/error.png" class="center-all-contens"><br>El NIT que ha ingresado ya esta registrado.<br>Por favor ingrese otro número de NIT';
-            }
+                echo '
+                    <img src="assets/img/error.png" class="center-all-contens"><br>El nombre de usuario que ha ingresado ya esta registrado.<br>Por favor ingrese otro número de NIT.
+                    <script>
+                        $("#img-captcha").attr("src","./captcha/Captcha.php");
+                    </script>
+                    ';
+            }   
         }
         else
         {
-            echo '<img src="assets/img/error.png" class="center-all-contens"><br>Error el captcha es incorrecto';
+            echo '
+                <img src="assets/img/error.png" class="center-all-contens"><br>Error el captcha es incorrecto.
+                <script>
+                    $("#img-captcha").attr("src","./captcha/Captcha.php");
+                </script>
+                ';
         }
     }
     else 
     {
-        echo '<img src="assets/img/error.png" class="center-all-contens"><br>Error los campos no deben de estar vacíos';
+        echo '
+            <img src="assets/img/error.png" class="center-all-contens"><br>Error los campos no deben de estar vacíos
+            <script>
+                $("#img-captcha").attr("src","./captcha/Captcha.php");
+            </script>
+        ';
     }
 ?>
