b51577a4a8803521c4b52b7d5d811384dd92222a
Alegtx <alejandroa453@gmail.com>
2020-12-08 22:42:01 -0400

    Historial de pedidos
    -Motrar un mensaje si el historial de pedidos esta vacio
    
    Co-Authored-By: CH3ITO <49537021+CH3ITO@users.noreply.github.com>

--- procesos/mostrarHistorial.php
+++ procesos/mostrarHistorial.php
@@ -12,55 +12,57 @@
     else
     {
     	$totalclientes = ejecutarSQL::consultar("select * from cliente where Usuario = '".$_GET["usuario"]."'");
-    	$CantidadPedidos = 0;
 	    while ($cliente = mysqli_fetch_array($totalclientes))
 	    {
-	    	$CantidadPedidos++;
 	    	$totalpedidos = ejecutarSQL::consultar("select * from venta where NIT = '".$cliente["NIT"]."' order by NumPedido desc");
-	    	echo '
-	    		<div class="table-responsive">
-			    	<table class="table table-bordered text-center>
-			            <thead class="">
-			                <tr>
-				                <th class="text-center">#</th>
-				                <th class="text-center">Fecha</th>
-				                <th class="text-center">Total</th>
-				                <th class="text-center">Estado</th>
-				                <th class="text-center">Fecha de entrega</th>
-				                <th class="text-center">Fecha de recogo</th>
-				                <th class="text-center">Opciones</th>
-			            	</tr>
-			            </thead>
-			            <tbody class="text-center">
-	    	';
-		    while($pedido = mysqli_fetch_array($totalpedidos))
-		    {
-		    	echo '	    		
-				    		<tr>
-				                <td>'.$pedido['NumPedido'].'</td>
-				                <td>'.$pedido['Fecha'].'</td>
-				                <td>'.$pedido['TotalPagar'].' Bs.</td>
-				                <td>'.$pedido['Estado'].'</td>
-				                <td>'.$pedido['FechaEntrega'].'</td>
-				                <td>'.$pedido['FechaRecogo'].'</td>
-			                    <td class="text-center">
-			                      	<div onClick="verDetallePedido('."'".$pedido['NumPedido']."'".')"
-			                      	class="btn btn-sm btn-info">Detalle</div>
-			                    </td>
-				            </tr>
-		            ';
-			}
-			echo "
-						</tbody>
-					</table>
-				</div>";
-	    }
-	    if($CantidadPedidos == 0)
-	    {
-	    	echo "
-	    		<p class='text-center'><i class='fa fa-shopping-cart fa-3x'></i><br>
-	    		Toadiva no ha realizado ningun pedido.</p>
-    		";
+	    	$cantfilas = mysqli_num_rows($totalpedidos);
+	    	if($cantfilas == 0)
+	    	{
+	    		echo "
+		    		<p class='text-center'><i class='fa fa-shopping-cart fa-3x'></i><br>
+		    		Toadiva no ha realizado ningun pedido.</p>
+	    		";
+	    	}
+	    	else
+	    	{
+	    		echo '
+		    		<div class="table-responsive">
+				    	<table class="table table-bordered text-center>
+				            <thead class="">
+				                <tr>
+					                <th class="text-center">#</th>
+					                <th class="text-center">Fecha</th>
+					                <th class="text-center">Total</th>
+					                <th class="text-center">Estado</th>
+					                <th class="text-center">Fecha de entrega</th>
+					                <th class="text-center">Fecha de recogo</th>
+					                <th class="text-center">Opciones</th>
+				            	</tr>
+				            </thead>
+				            <tbody class="text-center">
+		    	';
+			    while($pedido = mysqli_fetch_array($totalpedidos))
+			    {
+			    	echo '	    		
+					    		<tr>
+					                <td>'.$pedido['NumPedido'].'</td>
+					                <td>'.$pedido['Fecha'].'</td>
+					                <td>'.$pedido['TotalPagar'].' Bs.</td>
+					                <td>'.$pedido['Estado'].'</td>
+					                <td>'.$pedido['FechaEntrega'].'</td>
+					                <td>'.$pedido['FechaRecogo'].'</td>
+				                    <td class="text-center">
+				                      	<div onClick="verDetallePedido('."'".$pedido['NumPedido']."'".')"
+				                      	class="btn btn-sm btn-info">Detalle</div>
+				                    </td>
+					            </tr>
+			            ';
+				}
+				echo "
+							</tbody>
+						</table>
+					</div>";
+	    	}	
 	    }
     }
 ?>
